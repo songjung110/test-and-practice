d88f337f101ea2b2f762cbbf97e7c002724edf72
송정 <tjsjttt@gmail.com>
2022-09-16 15:48:38 +0900

    검색 폼 구현 2 강의까지 들은 후 주석으로 내용정리

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -7,19 +7,32 @@ const tag = '[FormView]';
 // 2. 각 파일마다 변수명이 tag 공통인걸 보아
 //    필요할 때에 어느 파일에서 불러와진건지 알기 쉬우라고일수도잇지않을가
 //    ,,,,,,,,,,,,,,,,,,,,,,maybe for the debugging,,,,,,,,,,,,,,,,,,,,,,
+// 강의에서 디버깅을 위한거라고 말해주심 추측한게 맞아서 기분좋음
 
 const FormView = Object.create(View); // 지정된 프로토타입 객체 및 속성을 갖는 새 객체를 만듦
-console.log(FormView);
 
+// setup메서드 - element 주입 받아서 내부적으로 속성을 갖게 함
 FormView.setup = function (el) {
-    this.init(el); // MainController 에서 넘겨준 form 태그를 여기서 사용함
+    this.init(el); // MainController 에서 넘겨준 form 태그를 init 함수를 이용해 this 전환되게 함
     this.inputEl = el.querySelector('[type=text]');
     this.resetEl = el.querySelector('[type=reset]');
-    this.showResetBtn();
+    this.showResetBtn(false);
+    this.bindEvents();
+    return this;
 };
-
-FormView.showResetBtn = function (show = false) {
+FormView.showResetBtn = function (show = true) {
     this.resetEl.style.display = show ? 'block' : 'none';
 };
+FormView.bindEvents = function () {
+    this.inputEl.addEventListener('keyup', (e) => this.onKeyup(e));
+};
+FormView.onKeyup = function () {
+    this.showResetBtn(this.inputEl.value.length); // length 0 값 자체가 false 로 계산되는 js라서 이렇게 처리가 가능한듯!!
+};
+// 여기까지 따라 쓰면서 ..................  form에 들어가야할 "기능"은 전부 FormView.js 안에서만 쓰여야만함
+// [추측임]
+// 1. 디버깅을 편하게 하기 위해?
+// 2. 코드의 깔끔함을 위해
+// 3. 유지보수 할 때에도 편할거같음 form 을 안쓴다는 조건 하에 해당하는 import만 주석처리 하면 되니까 ????
 
 export default FormView;
