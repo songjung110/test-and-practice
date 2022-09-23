53c102c3ea65c31526397cbc450a2edb82d0eb17
송정 <tjsjttt@gmail.com>
2022-09-23 14:43:25 +0900

    Array.from 잘못 작성한 내용 수정 후 커밋

--- 1-vanilla/js/views/TabView.js
+++ 1-vanilla/js/views/TabView.js
@@ -8,10 +8,10 @@ TabView.setup = function (el) {
 };
 
 TabView.setActiveTabs = function (tabName) {
-    Array.from(
-        this.el.querySelectorAll('li').forEach((li) => {
-            li.className = li.innerHTML === tabName ? 'active' : '';
-        })
-    );
+    console.log(this);
+    Array.from(this.el.querySelectorAll('li')).forEach((li) => {
+        li.className = li.innerHTML === tabName ? 'active' : '';
+    });
 };
+
 export default TabView;
